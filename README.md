0ea9c5aac5ac428ba86d972fbc5387a20b81e638
Hilton Shumway <hillshum@gmail.com>
2021-04-28 23:29:55 -0500

    Add README

---
+++ README.md
@@ -0,0 +1,11 @@
+# YNAB-Inkplate
+
+This is a simple project to display category balances from my YNAB budget on my
+Inkplate.
+
+
+## Usage
+
+Copy `ynab/arduino_secrets_example.h` to `ynab/arduino_secrets.h` and fill it it.
+
+Deploy to Inkplate in usual manner.
---
+++ ynab/arduino_secrets_example.h
@@ -0,0 +1,6 @@
+#define SECRET_SSID "<SSID>"
+#define SECRET_WIFI_PW "<WiFi credential>"
+#define SECRET_YNAB_KEY "<YNAB API key>"
+#define SECRET_CATEGORY_FUN "<category ID>"
+#define SECRET_CATEGORY_MEALS "<category ID>"
+#define SECRET_NUM_CATEGORIES 2 // leave that for now. Working on fixing this
